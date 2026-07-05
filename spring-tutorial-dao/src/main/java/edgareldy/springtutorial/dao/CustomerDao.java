e42eacc2231121041dec01001230e2472369f392
EdgarEldy <ed.eldy21@live.com>
2026-07-05 23:30:58 +0400

    feat(dao): add CustomerDao contract and implementation
    Plain CRUD, no relations of its own (Order points to Customer, not
    the other way around).

---
+++ spring-tutorial-dao/src/main/java/edgareldy/springtutorial/dao/CustomerDao.java
@@ -0,0 +1,24 @@
+package edgareldy.springtutorial.dao;
+
+import edgareldy.springtutorial.domain.Customer;
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * Contract for data access on Customer.
+ * <p>
+ * Created edgar.muhamyangabo on 7/6/26
+ * Author : edgar.muhamyangabo
+ * Date : 7/6/26
+ * Project : spring-tutorial
+ */
+public interface CustomerDao {
+
+    Optional<Customer> findById(Long id);
+
+    List<Customer> findAll(int page, int size);
+
+    Customer save(Customer customer);
+
+    void deleteById(Long id);
+}
